11699bcec93ee8cbd34c842bda16d6f7947a4975
Gabriel Cordoba <gabrielncordoba@gmail.com>
2023-05-15 20:47:45 -0300

    update syles in components

--- src/components/ImgBackground/Background.jsx
+++ src/components/ImgBackground/Background.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const ImgBackground = () => {
   return (
     <Image
       source={require('../../../images/backgroundPerrinci.jpg')}
-      style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+      style={styles.imagenBackground}
     >
     </Image>
   );
 }
+
+const styles = StyleSheet.create({
+  imagenBackground: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+
+});
 
 
 export default ImgBackground;
